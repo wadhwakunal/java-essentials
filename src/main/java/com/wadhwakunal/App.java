fbc91fd98d9745d9dfe7ca7f2ea32fdff5158b3e
kunwadhw2 <kunal.wadhwa@publicissapient.com>
2023-03-24 13:16:37 +0530

    Adding files for maven project

---
+++ src/main/java/com/wadhwakunal/App.java
@@ -0,0 +1,17 @@
+package com.wadhwakunal;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
